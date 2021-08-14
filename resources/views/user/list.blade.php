0a17291fcbe661a7b74ba5bba2674acebcf6b9a5
buruninho <r4rgn0r@gmail.com>
2021-08-14 13:24:52 -0300

    first steps - creating routes, controller and views

---
+++ resources/views/user/list.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.principal')
+
+@section('title', 'Listagem de usuários')
+@section('main_content')
+
+    <div>Listagem</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * Rota agrupada para realização de ações voltadas ao usuário:
+ * 
+ * Primeira rota seŕá responsável pelo visualização da página de cadastro;
+ * 
+ * Segunda rota será responsável pelo registro em si do novo usuário populando o banco de dados
+ * com informações provenientes do form via Request;
+ */
+Route::prefix('/usuarios')->name('usuario.')->group(function () {
+    
+    Route::get('/cadastro', UserController::class, 'create')->name('create');
+    Route::post('/store', UserController::class, 'store')->name('store');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
